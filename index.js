e8a83b2af3520a2f2aaa3ef8da2911aa2c3cd66e
Syed Hasnain <syedhasnainmurtaza@gmail.com>
2023-02-20 21:57:51 +0500

    combined frontend with backend

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
 
+app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 let notes = [
     {
